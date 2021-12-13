e253ad8866a7523ed77064965ad75207bc6505cc
RSalcido2019 <48775450+RSalcido2019@users.noreply.github.com>
2021-12-13 16:25:46 -0600

    SENDS SMS FROM DESKTOP

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
-import AllTask from "./views/AllTask";
+
 
 import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
-
 import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
 import  NewTask from "./views/NewTask";
+import AllTask from "./views/AllTask";
 // styles
 import "./App.css";
 
--- src/main/ui/src/views/AllTask.js
+++ src/main/ui/src/views/AllTask.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import {NewTask} from "./NewTask"
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,33 +6,24 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 
-
 export default function AllTask()  {
-   let taskArray = [{
-                        taskName: "1",
-                        description: "1",
-                        datetime: "1",
-                        reminder: "1",
-                        priority: "1",
-
-                      }]
+     const [taskList, setTaskList] = useState(() => {
+        // getting stored value
+        const saved = localStorage.getItem("taskList");
+        const initialValue = JSON.parse(saved);
+        return initialValue || "";
+      });
     return (
     <div>
-      {taskArray.map((task, idx) =>{
+      {taskList.map((task, idx) =>{
           return(
            <Card sx={{ minWidth: 275 }}>
                        <CardContent>
                          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                           {task.taskName}
                          </Typography>
-
-                         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                           adjective
-                         </Typography>
                          <Typography variant="body2">
-                           well meaning and kindly.
-                           <br />
-                           {'"a benevolent smile"'}
+                           {task.description}
                          </Typography>
                        </CardContent>
                        <CardActions>
